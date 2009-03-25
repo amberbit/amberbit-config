deba59d021c3f9ff109005f6009fed9175819348
Wojciech Piekutowski <wojciech@piekutowski.net>
2009-03-25 22:58:18 +0100

    Additional RSpec example added

--- spec/plugin_spec.rb
+++ spec/plugin_spec.rb
@@ -11,13 +11,20 @@ describe AmberBitAppConfig do
   it "should load values from default.yml" do
     AppConfig['app'].should be_instance_of(HashStruct)
     AppConfig['app']['name'].should eql("MyApp")
+    AppConfig.app.name.should eql("MyApp")
   end
 
   it "should overwrite values defined in default.yml by values defined in config.yml" do
-    AppConfig['current_file_name'].should eql("default.yml") 
+    AppConfig['current_file_name'].should eql("config.yml")
+    AppConfig.current_file_name.should eql("config.yml")
+  end
+
+  it "should overwrite values defined in default.yml by values defined in config.yml in nested keys" do
+    AppConfig.app.url.should eql("http://myapp.localhost")
   end
  
   it "should overwrite values defined in default section by values from current enviromnent section" do
-    AppConfig['current_env'].should eql("test")    
+    AppConfig['current_env'].should eql("test")
+    AppConfig.current_env.should eql("test")
   end
 end

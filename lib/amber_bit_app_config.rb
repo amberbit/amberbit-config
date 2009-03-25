bb06eec2ea7b75e7a3cbbb3881632e2628c4f491
Wojciech Piekutowski <wojciech@piekutowski.net>
2009-03-25 22:47:41 +0100

    HashStruct class moved to separate file

--- lib/amber_bit_app_config.rb
+++ lib/amber_bit_app_config.rb
@@ -1,13 +1,3 @@
-require "ostruct"
-
-# Defines HashStruct class, which is simply OpenStruct and has additional 
-# accessor mode via square brackets like hashes
-class HashStruct < OpenStruct
-  def [](key)
-    self.send key
-  end
-end
-
 # Defines class used for storing application-wide setting values.
 # == Usage:
 #
---
+++ lib/hash_struct.rb
@@ -0,0 +1,9 @@
+require "ostruct"
+
+# Defines HashStruct class, which is simply OpenStruct and has additional
+# accessor mode via square brackets like hashes
+class HashStruct < OpenStruct
+  def [](key)
+    self.send key
+  end
+end

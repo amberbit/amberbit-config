8d203719c238f9c9cf5a2577a380b0d286305056
Hubert Łępicki <hubert.lepicki@amberbit.com>
2012-02-08 17:43:12 +0100

    remove old version, prepare to be replaced by rewrite

--- lib/amberbit-config.rb
+++
@@ -1,3 +0,0 @@
-require 'yaml'
-require File.join(File.dirname(__FILE__), 'hash_struct')
-require File.join(File.dirname(__FILE__), 'amber_bit_app_config')
--- lib/hash_struct.rb
+++
@@ -1,9 +0,0 @@
-require "ostruct"
-
-# Defines HashStruct class, which is simply OpenStruct and has additional
-# accessor mode via square brackets like hashes
-class HashStruct < OpenStruct
-  def [](key)
-    self.send key unless key == nil
-  end
-end
